2b6127d11559b1295e4230b489a8b20b1f38d86e
harishgunasec <97063314+harishgunasec@users.noreply.github.com>
2022-04-14 13:16:41 +0530

    Update wishes.py

--- wishes.py
+++ wishes.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Happy Tamil New Year</h1>'
-      return '<h2>Aji</h2>'
-       return '<h3>Chitra</h3>'
-        return '<h3>Divya</h4>'
+    return '<h2>Aji</h2>'
+    return '<h3>Chitra</h3>'
+    return '<h3>Divya</h4>'
     return '<h5>Enjoy your day folks!!!</h5>'
