f9a51491ff06efb9299e772ae2647879cffb5de3
harishgunasec <97063314+harishgunasec@users.noreply.github.com>
2022-04-14 12:34:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<h1>Happy Tamil New Year</h1>'
+      return '<h2>Aji</h2>'
+       return '<h3>Chitra</h3>'
+        return '<h3>Divya</h4>'
+    return '<h5>Enjoy your day folks!!!</h5>'
